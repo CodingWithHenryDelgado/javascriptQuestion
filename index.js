f5bded790636f043078d00ef54a2128e67a9ac8a
Henry_DelGlitch <CodingWHenry@gmail.com>
2022-11-10 21:16:09 -0500

    Initial file

---
+++ index.js
@@ -0,0 +1,114 @@
+// Write a JavaScript program to get the difference between a given number and
+// 13, if the number is greater than 13 return double the absolute difference
+
+// Write a JavaScript program to check two given numbers and return true if one of
+// the number is 50 or if their sum is 50
+
+// Write a JavaScript program to compute the sum of the two given integers. If the
+// two values are same, then returns triple their sum.
+
+// Write a JavaScript program to check whether a given integer is within 20 of 100
+// or 400.
+
+// Write a JavaScript program to check from two given integers, whether one is
+// positive and another one is negative
+
+// Write a JavaScript program to check whether a given positive number is a
+// multiple of 3 or a multiple of 7.
+
+// Write a JavaScript program to create a new string from a given string taking the
+// last 3 characters and added at both the front and back. The string length must be
+// 3 or more.
+
+// Write a JavaScript program to check whether a string starts with 'Java' and false
+// otherwise
+
+// Write a JavaScript program to check whether two given integer values are in the
+// range 50..99 (inclusive). Return true if either of them are in the said range.
+
+// Write a JavaScript program to find the largest of three given integers.
+
+// Write a JavaScript program to find a value which is nearest to 100 from two
+// different given integer values.
+
+// Write a JavaScript program to find the larger number from the two given positive
+// integers, the two numbers are in the range 40..60 inclusive.
+
+// Write a program to check whether a specified character exists within the 2nd to
+// 4th position in a given string.
+
+// Write a JavaScript program to check whether the last digit of the three given
+// positive integers is same
+
+// Write a JavaScript program to create new string with first 3 characters are in
+// lower case from a given string. If the string length is less than 3 convert all the
+// characters in upper case.
+
+// Write a JavaScript program to check the total marks of a student in various
+// examinations. The student will get A+ grade if the total marks are in the range
+// 89..100 inclusive, if the examination is "Final-exam." the student will get A+ grade
+// and total marks must be greater than or equal to 90. Return true if the student get
+// A+ grade or false otherwise.
+
+// Write a JavaScript program to compute the sum of the two given integers, If the
+// sum is in the range 50..80 return 65 other wise return 80.
+
+// Write a JavaScript program to check three given numbers, if the three numbers
+// are same return 30 otherwise return 20 and if two numbers are same return 40.
+
+// Write a JavaScript program to check two given integer values and return true if
+// one of the number is 15 or if their sum or difference is 15.
+
+// Write a JavaScript program to check whether a given number is presents in the
+// range 40..10000.For example 40 presents in 40 and 4000
+
+// Write a JavaScript program to replace every character in a given string with the
+// character following it in the alphabet.
+
+// Write a JavaScript program to check whether the characters a and b are
+// separated by exactly 3 places anywhere (at least once) in a given string.
+
+// Write a JavaScript program to count the number of vowels in a given string.
+
+// Write a JavaScript program to create a new string without the first and last
+// character of a given string.
+
+// Write a JavaScript program to concatenate two strings except their first character.
+
+// Write a JavaScript program to test whether a string end with "Script". The string
+// length must be greater or equal to 6.
+
+// Write a JavaScript program to rotate the elements left of a given array of
+// integers of length 3.
+
+// Write a JavaScript program to check whether 1 appears in first or last position of
+// a given array of integers. The array length must be greater or equal to 1
+
+// Write a JavaScript program to find the larger value between the first or last and
+// set all the other elements with that value. Display the new array.
+
+// Write a JavaScript program to test whether an array of integers of length 2
+// contains 1 or a 3.
+
+// Write a JavaScript program to test whether a given array of integers contains 30
+// and 40 twice. The array length should be 0, 1, or 2.
+
+// Write a JavaScript to find the longest string from a given array of strings.
+
+// Write a JavaScript program to compute the sum of all digits that occur in a given
+// string.
+
+// Write a JavaScript function that accepts a string as a parameter and converts the
+// first letter of each word of the string in upper case.
+// Example string : 'the quick brown fox'
+// Expected Output : 'The Quick Brown Fox '
+
+// Write a JavaScript function that accepts a string as a parameter and counts the
+// number of vowels within the string.
+// Note : As the letter 'y' can be regarded as both a vowel and a consonant, we do
+// not count 'y' as vowel here.
+// Example string : 'The quick brown fox'
+// Expected Output : 5
+
+// Write a JavaScript function to get the number of occurrences of each letter in
+// specified string.
